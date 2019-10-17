99b831b1ec077e0dddee2d928ad2b8758804acfb
Mathieu Skúlason <mathieugrettir@hotmail.com>
2019-10-17 17:47:34 +0200

    Added the main xcode project.

---
+++ EISTI C++ Course/EISTI C++ Course/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EISTI C++ Course
+//
+//  Created by Mathieu MBP 13 on 17/10/2019.
+//  Copyright © 2019 Mathieu MBP 13. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
