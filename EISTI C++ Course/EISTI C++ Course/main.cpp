8678570c4f2db9316efbd9208b5537c8ebb0481e
Mathieu Skúlason <mathieugrettir@hotmail.com>
2019-10-17 20:37:16 +0200

    Added the logic to calculate a grade based on points and total points available on the exam on a scale of A-F of type char. We then use a switch statement to print out output based on the grade.

--- EISTI C++ Course/EISTI C++ Course/main.cpp
+++ EISTI C++ Course/EISTI C++ Course/main.cpp
@@ -12,21 +12,32 @@ using namespace std;
 
 /*
  In this program we will look at input and multiway if, else if,
- and else statements. We will simulate a shop where the reducation
- a buyer gets depends on the amount he shops for.
+ and else statements with an external function which will give a grade
+ based on the input value. We will then use the grade to print out different
+ information about the student with a switch statement.
  */
 
+/*
+ Function declaration so the compiler knows this function exists.
+ This function takes the first parameter points which indicates the points
+ the user scored on the exam. The total points is the number of points available
+ in total in the exam. Based on this we will return a grade on the scale of A-F
+ as a char (ASCII characters).
+ */
+char grade(int points, int totalPoints);
+
+/*
+ Calculates the percentatge score based on the points of the student scored divided
+ by the total points on the exam, multiplied by a hundred to give us a percentage but
+ not a fraction.
+ */
+double percentage(int points, int totalPoints);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     
-    // Spending in euros, no cents allowed.
-    double spending;
-    // The points the user gets based on their spending. They get 1 point for each
-    // euro they spend.
-    int pointsGained = 0;
-    // The price reducation the user gets
-    double reduction;
+    int studentPoints;
+    int totalExamPoints;
     
     // To make the outpout show a dot and 2 fractional characters
     // for doubles
@@ -34,32 +45,95 @@ int main(int argc, const char * argv[]) {
     cout.setf(ios::fixed);
     cout.precision(2);
     
-    cout << "Input how much you are willing to spend: ";
+    cout << "Student points: ";
+    cin >> studentPoints;
     
-    cin >> spending;
+    cout << "Total points available on exam: ";
+    cin >> totalExamPoints;
     
-    if (spending < 10) {
-        // If spending is less than 10 euros the user gets no price reduction.
-        // We need to set the reduction variable to 0 so it has some value.
-        reduction = 0;
-    } else if (spending >= 10 && spending <= 30) {
-        // If spending is 10 or more euros but less than or equal to 30 euros
-        // the user gets a 10% reduction.
-        reduction = spending * 0.1;
-    } else {
-        // If the user spends more than 30 euros they get a 20% reduction.
-        reduction = spending * 0.2;
+    // Add a check here if the total exam points are zero.
+    // As we cannot divide by zero.
+    
+    char studentGrade = grade(studentPoints, totalExamPoints);
+    
+    // Print different information depending on the grade of the statement
+    // Switch only works with integer types and enums.
+    switch (studentGrade) {
+        case 'A':
+            
+            cout << "You are a great student! You got an " << studentGrade << " on the exam" << endl;
+            
+            // If there is no break statement which stops the current control flow
+            // then it will keep outputting the statements in the next cases.
+            break;
+            
+        case 'B':
+            
+            cout << "You are a good student! You got a " << studentGrade << " on the exam" << endl;
+            
+            break;
+            
+        case 'C':
+            
+            cout << "You can do better... You got a " << studentGrade << " on the exam" << endl;
+            
+            break;
+            
+        case 'D':
+            
+            cout << "You are not in the worst bracket... but not far." << endl;
+            
+        case 'F':
+            
+            cout << "You need help! ASAP." << endl;
+            
+            break;
+            
+        default:
+            break;
     }
     
-    // By default int is rounded so we need to use a floor method to calculate the
-    // number of points the user gained
-    pointsGained += floor(spending);
+    cout << "Your grade was: " << grade(studentPoints, totalExamPoints) << endl;
+    cout << "Your exam score was: " << studentPoints;
+    cout << " for a total of: " << totalExamPoints << endl;
     
-    
-    cout << "The user spent: " << spending << endl;
-    cout << "He got a reduction worth: " << reduction << endl;
-    cout << "The user paid: " << spending - reduction << endl;
-    cout << "And gained " << pointsGained << " points" << endl;
+    cout << "Which makes your percentage " << percentage(studentPoints, totalExamPoints) << "%" << endl;
     
     return 0;
+}
+
+char grade(int points, int totalPoints) {
+    
+    double gradePerc = percentage(points, totalPoints);
+
+    if (gradePerc >= 90) {
+        
+        return 'A';
+    } else if (gradePerc >= 80 && gradePerc < 90) {
+        
+        return 'B';
+    } else if (gradePerc >= 70 && gradePerc < 80) {
+        
+        return 'C';
+    } else if (gradePerc >= 60 && gradePerc < 70) {
+        
+        return 'D';
+    } else {
+        
+        return 'F';
+    }
+    
+    // This should never execute as our control statement above ends with an else statement
+    // which is run if none of the other conditions are true.
+}
+
+double percentage(int points, int totalPoints) {
+    
+    // Calculates the percentage score on the exam.
+    double perc = (double)points / (double)totalPoints * 100;
+    
+    // Try removing the casting from the points. See what happens.
+    // Do you know why this occurs?
+    
+    return perc;
 }
