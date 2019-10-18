491495bbcc7a9bbb83dbd93d5cc1de08c5b936cb
Mathieu Skúlason <mathieugrettir@hotmail.com>
2019-10-18 09:21:25 +0200

    Added the argument prints for launch arguments as an example.

--- EISTI C++ Course/EISTI C++ Course/main.cpp
+++ EISTI C++ Course/EISTI C++ Course/main.cpp
@@ -20,46 +20,12 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
     
-    // Spending in euros, no cents allowed.
-    double spending;
-    // The points the user gets based on their spending. They get 1 point for each
-    // euro they spend.
-    int pointsGained = 0;
-    // The price reducation the user gets
-    double reduction;
+    cout << "Number of arguments: " << argc << endl;
     
-    // To make the outpout show a dot and 2 fractional characters
-    // for doubles
-    cout.setf(ios::showpoint);
-    cout.setf(ios::fixed);
-    cout.precision(2);
-    
-    cout << "Input how much you are willing to spend: ";
-    
-    cin >> spending;
-    
-    if (spending < 10) {
-        // If spending is less than 10 euros the user gets no price reduction.
-        // We need to set the reduction variable to 0 so it has some value.
-        reduction = 0;
-    } else if (spending >= 10 && spending <= 30) {
-        // If spending is 10 or more euros but less than or equal to 30 euros
-        // the user gets a 10% reduction.
-        reduction = spending * 0.1;
-    } else {
-        // If the user spends more than 30 euros they get a 20% reduction.
-        reduction = spending * 0.2;
+    for (int i = 0; i < argc; i++) {
+        const char *value = argv[i];
+        cout << "Value at index: " << i << " is: " << value << endl;
     }
-    
-    // By default int is rounded so we need to use a floor method to calculate the
-    // number of points the user gained
-    pointsGained += floor(spending);
-    
-    
-    cout << "The user spent: " << spending << endl;
-    cout << "He got a reduction worth: " << reduction << endl;
-    cout << "The user paid: " << spending - reduction << endl;
-    cout << "And gained " << pointsGained << " points" << endl;
     
     return 0;
 }
