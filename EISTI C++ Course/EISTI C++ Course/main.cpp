b0d58b2277d701f88da18ab8c80e7636e2f75316
Mathieu Skulason <mathieugrettir@hotmail.com>
2019-10-25 15:26:54 +0200

    Removed a bunch of code to make this run.

--- EISTI C++ Course/EISTI C++ Course/main.cpp
+++ EISTI C++ Course/EISTI C++ Course/main.cpp
@@ -11,55 +11,32 @@
 #include <math.h>
 #include <time.h>
 #include <vector>
+#include "Operation.hpp"
 using namespace std;
 
-
-/**
- Class enumerators are slightly different from normal enumerators.
- They are not integers and can only be compard with Operation enumerators.
- */
-typedef enum {
-    Rock,
-    Paper,
-    Scissor,
-    Lizard,
-    Spock
-} Operation;
-
-typedef enum {
-    Won,
-    Tie,
-    Lost
-} Result;
 
 typedef struct {
     Operation userOperation;
     Operation computerOperation;
-    Result userResult;
+    OperationResult userResult;
 } RoundHistory;
 
 bool playAgain();
 void printRules();
 Operation userOperation();
 Operation computerOperation();
-Result won(Operation operationOne, Operation operationTwo);
-void printOp(Operation op);
-void printResult(Result result);
 
 /* Copies the ctor passed in and prints the round number. */
 void printRoundResult(vector<RoundHistory> vec, int roundNumber);
 double percentage(double valueOne, double valueTwo);
 
 /** Creates a round History Struct element **/
-RoundHistory createHistoryDataPoint(Operation userOp, Operation computerOp, Result res);
+RoundHistory createHistoryDataPoint(Operation userOp, Operation computerOp, OperationResult res);
 
-int numUserResults(vector<RoundHistory> vec, Result result);
+int numUserResults(vector<RoundHistory> vec, OperationResult result);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    
-    // Initialize the random generator
-    srand(time(NULL));
 
     int roundNumber = 0;
     
@@ -77,8 +54,8 @@ int main(int argc, const char * argv[]) {
         roundNumber++;
         
         Operation userOp = userOperation();
-        Operation computerOp = computerOperation();
-        Result userWon = won(userOp, computerOp);
+        Operation computerOp = Operation::randOperation();
+        OperationResult userWon = userOp.won(computerOp);
         
         // Create a datapoint for the new round
         RoundHistory dataPoint = createHistoryDataPoint(userOp, computerOp, userWon);
@@ -137,20 +114,9 @@ Operation userOperation() {
     cin >> stringOperation;
     cin.ignore();
     
-    if (stringOperation == "r" || stringOperation == "R") {
-        
-        return Rock;
-    } else if (stringOperation == "p" || stringOperation == "P") {
-        
-        return Paper;
-    } else if (stringOperation == "s" || stringOperation == "S") {
-        
-        return Scissor;
-    } else if (stringOperation == "l" || stringOperation == "L") {
-        
-        return Lizard;
-    } else if (stringOperation == "sp" || stringOperation == "Sp" || stringOperation == "SP") {
-        return Spock;
+    OperationType type = Operation::operationType(stringOperation);
+    if (type != OperationTypeUnknown) {
+        return Operation(type);
     }
     
     cout << "Illegal input " << stringOperation << " try again." << endl;
@@ -158,97 +124,26 @@ Operation userOperation() {
     return userOperation();
 }
 
-Operation computerOperation() {
-    
-    Operation availableOperations[5] = { Rock, Paper, Scissor, Lizard, Spock };
-    
-    // Gives a random value from 0 - 4
-    int randomIndex = rand() % 5;
-    
-    return availableOperations[randomIndex];
-    
-}
-
-Result won(Operation operationOne, Operation operationTwo) {
-    
-    if (operationOne == operationTwo) {
-        return Tie;
-    }
-    
-    if (operationOne == Rock) {
-        if (operationTwo == Paper || operationTwo == Spock) {
-            return Lost;
-        }
-    } else if (operationOne == Paper) {
-        if (operationTwo == Scissor || operationTwo == Lizard) {
-            return Lost;
-        }
-    } else if (operationOne == Scissor) {
-        if (operationTwo == Rock || operationTwo == Spock) {
-            return Lost;
-        }
-    } else if (operationOne == Lizard) {
-        if (operationTwo == Rock || operationTwo == Scissor) {
-            return Lost;
-        }
-    } else if (operationOne == Spock) {
-        if (operationTwo == Paper || operationTwo == Lizard) {
-            return Lost;
-        }
-    }
-    
-    // We have exhausted all posibilities so we know we win.
-    return Won;
-}
-
 void printRoundResult(vector<RoundHistory> vec, int roundNumber) {
     
-    const RoundHistory lastRound = vec[roundNumber - 1];
+    RoundHistory lastRound = vec[roundNumber - 1];
     
     cout << "Round number: " << roundNumber << endl;
-    printOp(lastRound.userOperation);
+    lastRound.userOperation.printOperation();
     cout << " vs. ";
-    printOp(lastRound.computerOperation);
+    lastRound.computerOperation.printOperation();
     cout << ". ";
-    printResult(lastRound.userResult);
+    lastRound.userOperation.printResult( lastRound.computerOperation );
     cout << endl;
     
     // Print stats with other functions.
-    cout << "The user win percentage: " << percentage(numUserResults(vec, Won), roundNumber) << "%" << endl;
-    cout << "The computer win percentage: " << percentage(numUserResults(vec, Lost), roundNumber) << "%" << endl;
-    cout << "Number of ties: " << percentage(numUserResults(vec, Tie), roundNumber) << "%" << endl;
+    cout << "The user win percentage: " << percentage(numUserResults(vec, OperationResultWon), roundNumber) << "%" << endl;
+    cout << "The computer win percentage: " << percentage(numUserResults(vec, OperationResultLost), roundNumber) << "%" << endl;
+    cout << "Number of ties: " << percentage(numUserResults(vec, OperationResultTie), roundNumber) << "%" << endl;
     
 }
 
-void printResult(Result result) {
-    
-    if (result == Won) {
-        cout << "You won! Yayyy!" << endl;
-    } else if (result == Lost) {
-        cout << "You Lost! Better luck next time :)" << endl;
-    } else if (result == Tie) {
-        cout << "It's a tie!" << endl;
-    }
-    
-}
-
-void printOp(Operation op) {
-    
-    if (op == Rock) {
-        cout << "Rock";
-    } else if (op == Paper) {
-        cout << "Paper";
-    } else if (op == Scissor) {
-        cout << "Scissor";
-    } else if (op == Lizard) {
-        cout << "Lizard";
-    } else if (op == Spock) {
-        cout << "Spock";
-    }
-    
-}
-
-int numUserResults(vector<RoundHistory> vec, Result result) {
+int numUserResults(vector<RoundHistory> vec, OperationResult result) {
     
     int value = 0;
     
@@ -267,7 +162,7 @@ double percentage(double valueOne, double valueTwo) {
     return valueOne / valueTwo * 100;
 }
 
-RoundHistory createHistoryDataPoint(Operation userOp, Operation computerOp, Result res) {
+RoundHistory createHistoryDataPoint(Operation userOp, Operation computerOp, OperationResult res) {
     
     RoundHistory history;
     history.userOperation = userOp;
