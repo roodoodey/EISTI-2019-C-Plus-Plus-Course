b71a7d45e0b672e575563076b6bb8d040fdffc0a
Mathieu Skulason <mathieugrettir@hotmail.com>
2019-11-27 14:55:16 +0100

    Added brand new documentation.

--- EISTI C++ Course/EISTI C++ Course/main.cpp
+++ EISTI C++ Course/EISTI C++ Course/main.cpp
@@ -14,20 +14,28 @@ using namespace std;
 
 
 /**
- Class enumerators are slightly different from normal enumerators.
- They are not integers and can only be compard with Operation enumerators.
+ An Enumerator which identifies which operation we used.
  */
 typedef enum {
+    // Rock defaults to value 0
     Rock,
+    // Paper defaults to value 1
     Paper,
+    // Scissor defaults to value 2, etc.
     Scissor,
     Lizard,
     Spock
 } Operation;
 
+/**
+ An enumerator which identifies the result of two operations.
+ */
 typedef enum {
+    // Won defaults to 0
     Won,
-    Tie,
+    // Tie is set to 3
+    Tie=3,
+    // Lost is set to 4, because Lost is set to 3.
     Lost
 } Result;
 
@@ -37,17 +45,71 @@ typedef struct {
     Result userResult;
 } RoundHistory;
 
+/**
+A recursive method which asks the user after each round whether they want to play again.
+It returns true if they want to play again and false if they do not. I
+
+It is a recursive method which keeps calling itself until there is a valid input from the user.
+*/
 bool playAgain();
+
+/**
+ Uses cout to print the rules of the game in the console to the user on startup.
+ */
 void printRules();
+
+/**
+Asks the user which operation they want to use. If they use an unrecognized operation, it will recursively
+ask the user for an input until they give a valid one.
+*/
 Operation userOperation();
+
+/**
+This method creates a list of all of the operations, and then picks on operation at random
+for the computer which is then returned.
+*/
 Operation computerOperation();
+
+/**
+This method takes in two operations, and gives back the result based on the first operation.
+It sends the result based on the first operation. So if operation one is a scissor and operation
+two is a rock, the result will be Lost.
+*/
 Result won(Operation operationOne, Operation operationTwo);
+
+/**
+This method uses cout to print the name of the operatino to the console. So operation
+Rock prints Rock to the console.
+*/
 void printOp(Operation op);
+
+/**
+This method uses cout to print a message based on the result.
+*/
 void printResult(Result result);
+
+/**
+Prints the result of the round with the minimal statistics.
+*/
 void printRoundResult(RoundHistory *gameHistory, int roundNumber);
+
+/**
+Calculates the percentage of value one divided by value two.
+*/
 double percentage(double valueOne, double valueTwo);
+
+/**
+ Creates a new struct with a datapoint that represents a round.
+ */
 RoundHistory createHistoryDataPoint(Operation userOp, Operation computerOp, Result res);
+
+/**
+ Takes in the old list of rounds, with the new data point to add, creates a new dynamic array, copies the values from the listToCopy,
+ and adds the new data point in the end.
+ */
 RoundHistory * createHistoryArray(RoundHistory *listToCopy, RoundHistory newDataPoint, int numValues);
+
+
 int numUserResults(RoundHistory *history, Result result, int numValues);
 
 int main(int argc, const char * argv[]) {
@@ -60,23 +122,34 @@ int main(int argc, const char * argv[]) {
     
     // print the rules at the beginning
     printRules();
-    RoundHistory *historyArray = nullptr;
+    
+    // Start with an empty array
+    RoundHistory *historyArray = NULL;
     
     do {
         
         // Increment the round Number so we start at 1
         roundNumber++;
         
+        // The operation for the user
         Operation userOp = userOperation();
+        
+        // The operation for the computer chosen at random
         Operation computerOp = computerOperation();
+        
+        // Gets the result based on the operations.
         Result userWon = won(userOp, computerOp);
         
         // Create a datapoint for the new round
         RoundHistory dataPoint = createHistoryDataPoint(userOp, computerOp, userWon);
         
+        // Creates a new history array, by dynamically creating a new one, which has the size of the old list + 1
+        // It then copies all of the values from the old array and then adds the last value at the end of the array
+        // To store the result and moves made for each round.
         historyArray = createHistoryArray(historyArray, dataPoint, roundNumber);
         
         cout << endl;
+        // Prints the round result based on the history of the rounds and the current round number.
         printRoundResult(historyArray, roundNumber);
         
     } while ( playAgain() == true );
@@ -143,28 +216,38 @@ Operation userOperation() {
         return Spock;
     }
     
+    // If we get an unrecognized input we let the user know.
     cout << "Illegal input " << stringOperation << " try again." << endl;
     
+    // We then call the method again until the suer puts in a valid input.
     return userOperation();
 }
 
 Operation computerOperation() {
     
+    // Creates an array of size 5, with the values Rock, Paper, Scissor, Lizard, Spock as default values.
+    // When we have a fixed size we do not need to use the new operator. availableOperations is a pointer
+    // even though we do not have the star. All c-style arrays are pointers.
     Operation availableOperations[5] = { Rock, Paper, Scissor, Lizard, Spock };
     
     // Gives a random value from 0 - 4
     int randomIndex = rand() % 5;
     
+    // Send back the value at the random index in the availableOperations array.
     return availableOperations[randomIndex];
     
 }
 
 Result won(Operation operationOne, Operation operationTwo) {
     
+    // If we have the same operation we know it is a tie.
     if (operationOne == operationTwo) {
         return Tie;
     }
     
+    // We only check losing cases, if none of those are met
+    // We know we have a winning operation so we return
+    // the Win enumerator at the end.
     if (operationOne == Rock) {
         if (operationTwo == Paper || operationTwo == Spock) {
             return Lost;
@@ -203,9 +286,15 @@ void printRoundResult(RoundHistory *playerHistory, int roundNumber) {
     printResult(lastRound.userResult);
     cout << endl;
     
-    // Print stats with other functions.
+    // We print out the stats based on the history array by comparing with the appropriate results
+    
+    // Prints out the percentage of rounds won by the user, where the round number represents the number of rounds.
     cout << "The user win percentage: " << percentage(numUserResults(playerHistory, Won, roundNumber), roundNumber) << "%" << endl;
+    
+    // Prints out the percentage of rounds lost by the user, where the round number represents the number of rounds.
     cout << "The computer win percentage: " << percentage(numUserResults(playerHistory, Lost, roundNumber), roundNumber) << "%" << endl;
+    
+    // Prints out the percentage of rounds tied by the user, where the round number represents the number of rounds.
     cout << "Number of ties: " << percentage(numUserResults(playerHistory, Tie, roundNumber), roundNumber) << "%" << endl;
     
 }
@@ -238,12 +327,19 @@ void printOp(Operation op) {
     
 }
 
+/**
+ We pass in our history array with a result to compare to and check how many such results we have for the user.
+ This way we can see how often the user won, lost or was tied to the computer by passing in the appropriate result.
+ */
 int numUserResults(RoundHistory *history, Result result, int numValues) {
     
+    // The counter
     int value = 0;
     
+    // Loop through the history of made moves
     for (int i = 0; i < numValues; i++) {
         const RoundHistory point = history[i];
+        // If the comparison is met we increment the counter
         if (point.userResult == result) {
             value++;
         }
@@ -257,15 +353,23 @@ double percentage(double valueOne, double valueTwo) {
     return valueOne / valueTwo * 100;
 }
 
+/**
+ 
+ */
 RoundHistory createHistoryDataPoint(Operation userOp, Operation computerOp, Result res) {
     
+    // Initializes a struct with default values which can differ between compilers.
     RoundHistory history;
+    // You can initialize a history struc with the format below but the variables are declared in the same order as the struct
+    // So if the order of the struct changes, then you need to change it in your entire program.
+    // RoundHistory otherHistory = { userOp, computerOp, res };
     history.userOperation = userOp;
     history.computerOperation = computerOp;
     history.userResult = res;
     
     return history;
 }
+
 
 RoundHistory * createHistoryArray(RoundHistory *arrayToCopy, RoundHistory dataPoint, int numValues) {
     
@@ -287,6 +391,11 @@ RoundHistory * createHistoryArray(RoundHistory *arrayToCopy, RoundHistory dataPo
     // Decrease by one as arrays are zero indexed
     // Assing the new data point.
     list[numValues - 1] = dataPoint;
+    
+    // Since we have copied all of the values of the array to copy
+    // we can now delete the memory we allocated for it as all of the values
+    // are in our new list variable which we will return the pointer to.
+    delete[] arrayToCopy;
     
     return list;
 }
